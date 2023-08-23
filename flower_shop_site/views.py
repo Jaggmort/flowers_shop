d97182a04fa1059a292ee8b6836665d6f1279e0f
MrSabin <sabin.21011986@gmail.com>
2023-08-24 00:05:52 +0300

    Linked pages

--- flower_shop_site/views.py
+++ flower_shop_site/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
 
+
 def index(request):
-    context ={}
+    context = {}
     return render(request, 'index.html', context)
 
+
+def show_catalog(request):
+    context = {}
+    return render(request, 'catalog.html', context)
+
+
+def quiz_step(request):
+    return render(request, 'quiz-step.html')
+
+
+def quiz(request):
+    return render(request, 'quiz.html')
+
+
+def show_quiz_result(request):
+    return render(request, 'result.html')
+
+
+def make_order(request):
+    return render(request, 'order.html')
+
+
+def order_consultation(request):
+    return render(request, 'consultation.html')
