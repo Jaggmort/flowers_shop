440f1b42648f41c64e53716f7519339909491647
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-08-23 22:10:12 +0300

    Модели добавлены в админку

--- flower_shop_site/admin.py
+++ flower_shop_site/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Bouquet, Tag, Order, Delivery, Consultation
 
-# Register your models here.
+
+@admin.register(Bouquet)
+class BouquetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Consultation)
+class ConsultationAdmin(admin.ModelAdmin):
+    pass
