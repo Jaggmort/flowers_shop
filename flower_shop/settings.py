548330c887648d071dd24437889bb2e2518e1b48
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-08-23 22:10:43 +0300

    Добавлены зависимости и настройки для корректной работы моделей

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -31,8 +31,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'debug_toolbar',
     'flower_shop_site.apps.FlowerShopSiteConfig',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,9 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
